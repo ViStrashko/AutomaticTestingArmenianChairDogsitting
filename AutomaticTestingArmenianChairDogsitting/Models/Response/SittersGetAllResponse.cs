0f99155066a94aec2d5730960f661bb0a80de2e0
baikovIlya <baikusha23@gmail.com>
2022-08-03 02:23:57 +0300

    Add Model for Get/Sitters

---
+++ AutomaticTestingArmenianChairDogsitting/Models/Response/SittersGetAllResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace AutomaticTestingArmenianChairDogsitting.Models.Response
+{
+    public class SittersGetAllResponse
+    {
+        public class SitterAllInfoResponseModel
+        {
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            [JsonPropertyName("lastName")]
+            public string LastName { get; set; }
+
+            [JsonPropertyName("experience")]
+            public int Experience { get; set; }
+
+
+            public override bool Equals(object? obj)
+            {
+                return obj is SitterAllInfoResponseModel model &&
+                       Name == model.Name &&
+                       LastName == model.LastName &&
+                       Experience == model.Experience;
+            }
+        }
+
+    }
+}
