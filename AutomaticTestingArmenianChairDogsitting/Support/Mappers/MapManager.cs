fd3cf029820226a219982845f7ded1245c12159f
baikovIlya <baikusha23@gmail.com>
2022-08-03 03:35:46 +0300

    Add mapper

---
+++ AutomaticTestingArmenianChairDogsitting/Support/Mappers/MapManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomaticTestingArmenianChairDogsitting.Models.Request;
+using AutomaticTestingArmenianChairDogsitting.Models.Response;
+
+namespace AutomaticTestingArmenianChairDogsitting.Support.Mappers
+{
+    public class MapManager
+    {
+        
+        //public AuthRequestModel MapRegistModelToAuthModel(SitterRegistrationRequestModel sitter)
+        //{
+        //    SittersMapper mapper = new SittersMapper();
+        //    mapper.
+        //}
+    }
+}
---
+++ AutomaticTestingArmenianChairDogsitting/Support/Mappers/SittersMapper.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using AutomaticTestingArmenianChairDogsitting.Models.Request;
+using AutomaticTestingArmenianChairDogsitting.Models.Response;
+
+
+namespace AutomaticTestingArmenianChairDogsitting.Support.Mappers
+{
+    public class SittersMapper
+    {
+        //private static Mapper _instance;
+
+        //public static Mapper GetInstance()
+        //{
+        //    if (_instance == null)
+        //        InitializeInstance();
+        //    return _instance;
+        //}
+
+        //private static void InitializeInstance()
+        //{
+        //    _instance = new Mapper(new MapperConfiguration(cfg =>
+        //    {
+        //        cfg.CreateMap<AuthRequestModel, SitterRegistrationRequestModel>();
+        //    }));
+        //}
+        public AuthRequestModel MapSitterRegistModelToAuthModel(SitterRegistrationRequestModel sitter)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<AuthRequestModel, SitterRegistrationRequestModel>());
+            Mapper mapper = new Mapper(config);
+            var responseModel = mapper.Map<AuthRequestModel>(sitter);
+            return responseModel;
+        }
+        public SittersGetAllResponse MapSitterRegistModelToSitterGetAllResponse(SitterRegistrationRequestModel sitter, int id)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<SittersGetAllResponse, SitterRegistrationRequestModel>());
+            Mapper mapper = new Mapper(config);
+            SittersGetAllResponse responseModel = mapper.Map<SittersGetAllResponse>(sitter);
+            responseModel.Id = id;
+            return responseModel;
+        }
+    }
+}
