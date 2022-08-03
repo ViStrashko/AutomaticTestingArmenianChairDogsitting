0544a6ec2ff40626e76bbe8327e56b4c2bc10568
baikovIlya <baikusha23@gmail.com>
2022-08-03 03:36:45 +0300

    Change models

--- AutomaticTestingArmenianChairDogsitting/Models/Response/SittersGetAllResponse.cs
+++ AutomaticTestingArmenianChairDogsitting/Models/Response/SittersGetAllResponse.cs
@@ -9,28 +9,25 @@ namespace AutomaticTestingArmenianChairDogsitting.Models.Response
 {
     public class SittersGetAllResponse
     {
-        public class SitterAllInfoResponseModel
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("lastName")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("experience")]
+        public int Experience { get; set; }
+
+
+        public override bool Equals(object? obj)
         {
-            [JsonPropertyName("id")]
-            public int Id { get; set; }
-
-            [JsonPropertyName("name")]
-            public string Name { get; set; }
-
-            [JsonPropertyName("lastName")]
-            public string LastName { get; set; }
-
-            [JsonPropertyName("experience")]
-            public int Experience { get; set; }
-
-
-            public override bool Equals(object? obj)
-            {
-                return obj is SitterAllInfoResponseModel model &&
-                       Name == model.Name &&
-                       LastName == model.LastName &&
-                       Experience == model.Experience;
-            }
+            return obj is SitterAllInfoResponseModel model &&
+                   Name == model.Name &&
+                   LastName == model.LastName &&
+                   Experience == model.Experience;
         }
 
     }
