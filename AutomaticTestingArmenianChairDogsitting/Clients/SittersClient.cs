92d5c04785a8a9de0b7403c2304ed8ca9448008d
baikovIlya <baikusha23@gmail.com>
2022-08-09 05:44:56 +0300

    Add GetAllSitters Request

--- AutomaticTestingArmenianChairDogsitting/Clients/SittersClient.cs
+++ AutomaticTestingArmenianChairDogsitting/Clients/SittersClient.cs
@@ -30,6 +30,21 @@ namespace AutomaticTestingArmenianChairDogsitting.Clients
             return response.Content;
         }
 
+        public HttpContent GetAllSitters(string token, HttpStatusCode expectedCode)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpRequestMessage message = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new System.Uri($"{Urls.Sitters}")
+            };
+            HttpResponseMessage response = client.Send(message);
+            HttpStatusCode actualCode = response.StatusCode;
+            Assert.AreEqual(expectedCode, actualCode);
+            return response.Content;
+        }
+
         public HttpContent GetAllInfoSitterById(int id, string token, HttpStatusCode expectedCode)
         {
             HttpClient client = new HttpClient();
