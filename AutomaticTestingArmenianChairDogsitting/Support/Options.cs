d7fd5ba5c2635c8b9e95c4b5bb5ef724efe6fd76
baikovIlya <baikusha23@gmail.com>
2022-08-09 05:45:48 +0300

    Add Admins Email and Pass to Options

--- AutomaticTestingArmenianChairDogsitting/Support/Options.cs
+++ AutomaticTestingArmenianChairDogsitting/Support/Options.cs
@@ -4,5 +4,7 @@
     {
         public const string connectionString = @"Data Source = 80.78.240.16;Initial Catalog = ArmenianChairDogsitting.DB;Persist Security Info = True;User ID = student;Password = qwe!23;";
         public const string propertyBreedLarge = "Крупная";
+        public const string adminEmail = "admin@mail.ru";
+        public const string adminPassword = "user";
     }
 }
