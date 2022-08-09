62171714538386098ec136d0d3c4ef7be9960ab1
baikovIlya <baikusha23@gmail.com>
2022-08-09 05:46:54 +0300

    Add GetAllSittersByAllRoles Test

---
+++ AutomaticTestingArmenianChairDogsitting/Tests/TestSources/ViewTestSources/GetAllSittersTestSource.cs
@@ -0,0 +1,75 @@
+﻿using AutomaticTestingArmenianChairDogsitting.Models.Request;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AutomaticTestingArmenianChairDogsitting.Tests.TestSources.ViewTestSources
+{
+    public class GetAllSittersByAnyRoleTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            List<SitterRegistrationRequestModel> sitters = new List<SitterRegistrationRequestModel>()
+            {
+                new SitterRegistrationRequestModel()
+                {
+                    Name = "Валера",
+                    LastName = "Пет1",
+                    Phone = "+79514125547",
+                    Email = "pet0@gmail.com",
+                    Password = "12345678",
+                    Age = 20,
+                    Experience = 10,
+                    Sex = 1,
+                    Description = "Description"
+                },
+                new SitterRegistrationRequestModel()
+                {
+                    Name = "Валера",
+                    LastName = "Пет",
+                    Phone = "+79514125547",
+                    Email = "pet1@gmail.com",
+                    Password = "12345678",
+                    Age = 20,
+                    Experience = 10,
+                    Sex = 1,
+                    Description = "Description"
+                },
+                new SitterRegistrationRequestModel()
+                {
+                    Name = "Валера",
+                    LastName = "Пет",
+                    Phone = "+79514125547",
+                    Email = "pet2@gmail.com",
+                    Password = "12345678",
+                    Age = 20,
+                    Experience = 10,
+                    Sex = 1,
+                    Description = "Description"
+                },
+                new SitterRegistrationRequestModel()
+                {
+                    Name = "Валера",
+                    LastName = "Пет",
+                    Phone = "+79514125547",
+                    Email = "pet3@gmail.com",
+                    Password = "12345678",
+                    Age = 20,
+                    Experience = 10,
+                    Sex = 1,
+                    Description = "Description"
+                }
+            };
+            ClientRegistrationRequestModel clientModel = new ClientRegistrationRequestModel()
+            {
+                Name = "Вася",
+                LastName = "Петров",
+                Email = "petrov@gmail.com",
+                Phone = "+79514125547",
+                Address = "ул. Итальянская, дом. 10",
+                Password = "12345678",
+            };
+
+            yield return new object[] { sitters, clientModel };
+        }
+    }
+}
