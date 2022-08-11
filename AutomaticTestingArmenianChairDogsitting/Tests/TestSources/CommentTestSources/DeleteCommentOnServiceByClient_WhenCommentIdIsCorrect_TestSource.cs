c4dd6db966017b39be62d19ee01f09fbcf5073ce
ViStrashko <voringir@mail.ru>
2022-08-11 03:26:55 +0300

    Create CommentMappers and LeaveCommentOnServiceByClient

---
+++ AutomaticTestingArmenianChairDogsitting/Tests/TestSources/CommentTestSources/DeleteCommentOnServiceByClient_WhenCommentIdIsCorrect_TestSource.cs
@@ -0,0 +1,17 @@
+﻿using AutomaticTestingArmenianChairDogsitting.Models.Request;
+using System.Collections;
+
+namespace AutomaticTestingArmenianChairDogsitting.Tests.TestSources.CommentTestSources
+{
+    public class DeleteCommentOnServiceByClient_WhenCommentIdIsCorrect_TestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new CommentRegistrationRequestModel()
+            {
+                Rating = 5,
+                Text = "Собачка была под хорошим присмотром, и я не порвала себе сердце от беспокойства за неё.",
+            };
+        }
+    }
+}
---
+++ AutomaticTestingArmenianChairDogsitting/Tests/TestSources/CommentTestSources/LeaveCommentOnServiceByClient_WhenCommentModelIsCorrect_TestSource.cs
@@ -0,0 +1,17 @@
+﻿using AutomaticTestingArmenianChairDogsitting.Models.Request;
+using System.Collections;
+
+namespace AutomaticTestingArmenianChairDogsitting.Tests.TestSources.CommentTestSources
+{
+    public class LeaveCommentOnServiceByClient_WhenCommentModelIsCorrect_TestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new CommentRegistrationRequestModel()
+            {
+                Rating = 5,
+                Text = "Собачка была под хорошим присмотром, и я не порвала себе сердце от беспокойства за неё.",
+            };
+        }
+    }
+}
