aced225ba42beb29911a58682e04d23c18c26bb6
baikovIlya <baikusha23@gmail.com>
2022-08-13 05:05:38 +0300

    Add admins classes

---
+++ AutomaticTestingArmenianChairDogsitting/Steps/AdminSteps.cs
@@ -0,0 +1,26 @@
+﻿using AutomaticTestingArmenianChairDogsitting.Clients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomaticTestingArmenianChairDogsitting.Steps
+{
+    public class AdminSteps
+    {
+        private AdminClient _adminClient;
+        
+        public AdminSteps()
+        {
+            _adminClient = new AdminClient();
+        }
+
+        public void RestoreSittersProfileTest(int sitterId, string adminToken)
+        {
+            HttpStatusCode expectedCode = HttpStatusCode.NoContent;
+            _adminClient.RestoreSitter(sitterId, adminToken, expectedCode);
+        }
+    }
+}
---
+++ AutomaticTestingArmenianChairDogsitting/Tests/PositiveTests/AdminsTests.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using AutomaticTestingArmenianChairDogsitting.Models.Request;
+using AutomaticTestingArmenianChairDogsitting.Models.Response;
+using AutomaticTestingArmenianChairDogsitting.Steps;
+using System;
+using AutomaticTestingArmenianChairDogsitting.Support;
+using AutomaticTestingArmenianChairDogsitting.Support.Mappers;
+using AutomaticTestingArmenianChairDogsitting.Tests.TestSources.ClientTestSources;
+using AutomaticTestingArmenianChairDogsitting.Tests.TestSources.SitterTestSources;
+using System.Collections.Generic;
+
+namespace AutomaticTestingArmenianChairDogsitting.Tests.PositiveTests
+{
+    public class AdminsTests
+    {
+        private Authorizations _authorization;
+        private ClientSteps _clientSteps;
+        private SitterSteps _sitterSteps;
+        private ClearingTables _clearingTables;
+        private AuthMappers _authMapper;
+        private ClientMappers _clientMappers;
+        private SitterMappers _sitterMappers;
+        private string _clientToken;
+        private string _sitterToken;
+        private int _clientId;
+        private int _sitterId;
+        private string _adminToken;
+        private ClientRegistrationRequestModel _clientModel;
+        private SitterRegistrationRequestModel _sitterModel;
+        
+
+        public AdminsTests()
+        {
+            _authorization = new Authorizations();
+            _clientSteps = new ClientSteps();
+            _sitterSteps = new SitterSteps();
+            _clearingTables = new ClearingTables();
+            _authMapper = new AuthMappers();
+            _clientMappers = new ClientMappers();
+            _sitterMappers = new SitterMappers();
+
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _clearingTables.ClearAllDB();
+            _adminToken = _authorization.AuthorizeTest(new AuthRequestModel { Email = Options.adminEmail, Password = Options.adminPassword });
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _clientModel = new ClientRegistrationRequestModel()
+            {
+                Name = "Вася",
+                LastName = "Петров",
+                Email = "petrov@gmail.com",
+                Phone = "+79514125547",
+                Address = "ул. Итальянская, дом. 10",
+                Password = "12345678",
+                Promocode = ""
+            };
+            _clientId = _clientSteps.RegisterClientTest(_clientModel);
+
+            AuthRequestModel authClientModel = _authMapper.MappClientRegistrationRequestModelToAuthRequestModel(_clientModel);
+            _clientToken = _authorization.AuthorizeTest(authClientModel);
+
+            _sitterModel = new SitterRegistrationRequestModel()
+            {
+                Name = "Валера",
+                LastName = "Пет",
+                Phone = "+79514125547",
+                Email = "pet@gmail.com",
+                Password = "87654321",
+                Age = 20,
+                Experience = 10,
+                Sex = 1,
+                Description = "Description",
+                PriceCatalog = new List<PriceCatalogRequestModel>()
+                {
+                    new PriceCatalogRequestModel() { Service = 1, Price = 500 },
+                }
+            };
+            _sitterId = _sitterSteps.RegisterSitterTest(_sitterModel);
+
+            AuthRequestModel authSitterModel = _authMapper.MappSitterRegistrationRequestModelToAuthRequestModel(_sitterModel);
+            _sitterToken = _authorization.AuthorizeTest(authSitterModel);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _clearingTables.ClearAllDB();
+        }
+
+        [Test]
+        public void RestoreSittersProfileTest_ByAdmin_ShouldRestoreProfile(int id)
+        {
+
+        }
+    }
+}
