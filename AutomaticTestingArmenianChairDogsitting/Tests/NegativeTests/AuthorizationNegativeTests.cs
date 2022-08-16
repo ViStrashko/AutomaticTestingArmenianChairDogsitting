61134ab6bbcea30b0b0ce1a0414c5fb35fc7754f
ViStrashko <voringir@mail.ru>
2022-08-16 21:13:27 +0300

    Create ClientAuthorizationNegativeTest

--- AutomaticTestingArmenianChairDogsitting/Tests/NegativeTests/AuthorizationNegativeTests.cs
+++ AutomaticTestingArmenianChairDogsitting/Tests/NegativeTests/AuthorizationNegativeTests.cs
@@ -3,6 +3,7 @@ using AutomaticTestingArmenianChairDogsitting.Models.Request;
 using AutomaticTestingArmenianChairDogsitting.Steps;
 using AutomaticTestingArmenianChairDogsitting.Support;
 using AutomaticTestingArmenianChairDogsitting.Tests.NegativeTestSources.ClientNegativeTestSources;
+using AutomaticTestingArmenianChairDogsitting.Tests.NegativeTestSources.SitterNegativeTestSources;
 
 namespace AutomaticTestingArmenianChairDogsitting.Tests.NegativeTests
 {
@@ -10,12 +11,14 @@ namespace AutomaticTestingArmenianChairDogsitting.Tests.NegativeTests
     {
         private Authorizations _authorization;
         private ClientSteps _clientSteps;
+        private SitterSteps _sitterSteps;
         private ClearingTables _clearingTables;
 
         public AuthorizationNegativeTests()
         {
             _authorization = new Authorizations();
             _clientSteps = new ClientSteps();
+            _sitterSteps = new SitterSteps();
             _clearingTables = new ClearingTables();
         }
 
@@ -31,8 +34,9 @@ namespace AutomaticTestingArmenianChairDogsitting.Tests.NegativeTests
             _clearingTables.ClearAllDB();
         }
 
+        //Client
         [TestCaseSource(typeof(ClientAuthorizationNegativeTest_WhenClientIsRegisteredAndPasswordAndEmailIsStrangers_TetsSource))]
-        public void ClientAuthorizationNegativeTest_WhenClientIsRegisteredAndPasswordAndEmailIsStrangers_ShouldGetHttpStatusCodeUnprocessableEntity
+        public void ClientAuthorizationNegativeTest_WhenClientIsRegisteredAndPasswordAndEmailIsStrangers_ShouldGetHttpStatusCodeUnauthorized
             (ClientRegistrationRequestModel clientModel, AuthRequestModel authModel)
         {
             _clientSteps.RegisterClientTest(clientModel);
@@ -55,9 +59,26 @@ namespace AutomaticTestingArmenianChairDogsitting.Tests.NegativeTests
         }
 
         [TestCaseSource(typeof(ClientAuthorizationNegativeTest_WhenClientIsNotRegisteredAndDataIsNotCorrect_TetsSource))]
-        public void ClientAuthorizationNegativeTest_WhenClientIsNotRegisteredAndDataIsNotCorrect_ShouldGetHttpStatusCodeUnauthorized
+        public void ClientAuthorizationNegativeTest_WhenClientIsNotRegisteredAndDataIsNotCorrect_ShouldGetHttpStatusCodeUnprocessableEntity
             (AuthRequestModel authModel)
         {
+            _authorization.AuthorizeWhenPasswordOrEmailIsNotCorrectNegativeTest(authModel);
+        }
+
+        //Sitter
+        [TestCaseSource(typeof(SitterAuthorizationNegativeTest_WhenSitterIsRegisteredAndPasswordAndEmailIsStrangers_TetsSource))]
+        public void SitterAuthorizationNegativeTest_WhenSitterIsRegisteredAndPasswordAndEmailIsStrangers_ShouldGetHttpStatusCodeUnauthorized
+            (SitterRegistrationRequestModel sitterModel, AuthRequestModel authModel)
+        {
+            _sitterSteps.RegisterSitterTest(sitterModel);
+            _authorization.AuthorizeWhenAuthenticationFailedNegativeTest(authModel);
+        }
+
+        [TestCaseSource(typeof(SitterAuthorizationNegativeTest_WhenSitterIsRegisteredAndPasswordAndEmailIsNotCorrectt_TetsSource))]
+        public void SitterAuthorizationNegativeTest_WhenSitterIsRegisteredAndPasswordAndEmailIsNotCorrect_ShouldGetHttpStatusCodeUnprocessableEntity
+            (SitterRegistrationRequestModel sitterModel, AuthRequestModel authModel)
+        {
+            _sitterSteps.RegisterSitterTest(sitterModel);
             _authorization.AuthorizeWhenPasswordOrEmailIsNotCorrectNegativeTest(authModel);
         }
     }
